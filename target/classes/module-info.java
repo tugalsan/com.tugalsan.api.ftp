e717c79795af2e117ca4f16eed29564f6bea80e1
tugalsan karabacak <tugalsan@gmail.com>
2023-04-17 16:34:59 +0300

    .

--- target/classes/module-info.java
+++ target/classes/module-info.java
@@ -1,8 +1,8 @@
 module com.tugalsan.api.ftp {
     requires org.apache.commons.net;
     requires com.tugalsan.api.unsafe;
-    requires com.tugalsan.api.compiler;
-    requires com.tugalsan.api.executable;
+    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.runnable;
     requires com.tugalsan.api.list;
     requires com.tugalsan.api.stream;
     requires com.tugalsan.api.file;
