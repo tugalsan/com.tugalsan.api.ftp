8b60f0847b3bb6b901a09022d4204499044daf14
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-04-09 08:34:04 +0300

    .

--- src/main/java/com/tugalsan/api/ftp/server/TS_FtpUtils.java
+++ src/main/java/com/tugalsan/api/ftp/server/TS_FtpUtils.java
@@ -3,12 +3,11 @@ package com.tugalsan.api.ftp.server;
 import com.tugalsan.api.file.server.TS_FileUtils;
 import com.tugalsan.api.list.client.TGS_ListUtils;
 import com.tugalsan.api.log.server.TS_Log;
-import com.tugalsan.api.stream.client.TGS_StreamUtils;
-import com.tugalsan.api.unsafe.client.TGS_UnSafe;
+import com.tugalsan.api.union.client.TGS_Union;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Optional;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPReply;
 
@@ -16,73 +15,76 @@ public class TS_FtpUtils {
 
     final private static TS_Log d = TS_Log.of(TS_FtpUtils.class);
 
-    public static Optional<String> fetchWorkingDirectory(FTPClient ftpClient) {
-        return TGS_UnSafe.call(() -> {
-            return Optional.of(ftpClient.printWorkingDirectory());
-        }, e -> {
-            e.printStackTrace();
-            return Optional.empty();
-        });
+    public static TGS_Union<String> fetchWorkingDirectory(FTPClient ftpClient) {
+        try {
+            return TGS_Union.of(ftpClient.printWorkingDirectory());
+        } catch (IOException ex) {
+            return TGS_Union.ofExcuse(ex);
+        }
     }
 
-    public static boolean makeDirectory(FTPClient ftpClient, CharSequence newDir_withSlashPrefix) {
-        return TGS_UnSafe.call(() -> {
-            return ftpClient.makeDirectory(newDir_withSlashPrefix.toString());
-        }, e -> {
-            e.printStackTrace();
-            return false;
-        });
+    public static TGS_Union<Boolean> makeDirectory(FTPClient ftpClient, CharSequence newDir_withSlashPrefix) {
+        try {
+            return TGS_Union.of(ftpClient.makeDirectory(newDir_withSlashPrefix.toString()));
+        } catch (IOException ex) {
+            return TGS_Union.ofExcuse(ex);
+        }
     }
 
-    public static boolean upload(FTPClient ftpClient, Path file) {
-        return TGS_UnSafe.call(() -> {
-            return ftpClient.storeFile(TS_FileUtils.getNameFull(file), Files.newInputStream(file));
-        }, e -> {
-            e.printStackTrace();
-            return false;
-        });
+    public static TGS_Union<Boolean> upload(FTPClient ftpClient, Path file) {
+        try {
+            return TGS_Union.of(ftpClient.storeFile(TS_FileUtils.getNameFull(file), Files.newInputStream(file)));
+        } catch (IOException ex) {
+            return TGS_Union.ofExcuse(ex);
+        }
     }
 
-    public static boolean changeWorkingDirectory(FTPClient ftpClient, CharSequence newDir_withSlashPrefix) {
-        return TGS_UnSafe.call(() -> {
-            return ftpClient.changeWorkingDirectory(newDir_withSlashPrefix.toString());
-        }, e -> {
-            e.printStackTrace();
-            return false;
-        });
+    public static TGS_Union<Boolean> changeWorkingDirectory(FTPClient ftpClient, CharSequence newDir_withSlashPrefix) {
+        try {
+            return TGS_Union.of(ftpClient.changeWorkingDirectory(newDir_withSlashPrefix.toString()));
+        } catch (IOException ex) {
+            return TGS_Union.ofExcuse(ex);
+        }
     }
 
-    public static void destroy(FTPClient ftpClient) {
-        TGS_UnSafe.run(() -> ftpClient.logout(), e -> TGS_StreamUtils.runNothing());
-        TGS_UnSafe.run(() -> ftpClient.disconnect(), e -> TGS_StreamUtils.runNothing());
+    public static TGS_Union<Boolean> destroy(FTPClient ftpClient) {
+        try {
+            ftpClient.logout();
+        } catch (IOException ex) {
+            //DO NOTHING
+        }
+        try {
+            ftpClient.disconnect();
+            return TGS_Union.of(true);
+        } catch (IOException ex) {
+            return TGS_Union.ofExcuse(ex);
+        }
     }
 
-    public static Optional<FTPClient> connect(CharSequence hostName, int port) {
-        return TGS_UnSafe.call(() -> {
+    public static TGS_Union<FTPClient> connect(CharSequence hostName, int port) {
+        try {
             var ftpClient = new FTPClient();
             ftpClient.connect(hostName.toString(), port);
             var replyCode = ftpClient.getReplyCode();
             if (!FTPReply.isPositiveCompletion(replyCode)) {
-                TGS_UnSafe.thrw(d.className, "connect", "Operation failed. Server reply code: " + replyCode);
+                TGS_Union.ofExcuse(d.className, "connect", "Operation failed. Server reply code: " + replyCode);
             }
-            return Optional.of(ftpClient);
-        }, e -> {
-            e.printStackTrace();
-            return Optional.empty();
-        });
+            return TGS_Union.of(ftpClient);
+        } catch (IOException ex) {
+            return TGS_Union.ofExcuse(ex);
+        }
     }
 
-    public static boolean login(FTPClient ftpClient, CharSequence user, CharSequence pass) {
-        return TGS_UnSafe.call(() -> {
+    public static TGS_Union<Boolean> login(FTPClient ftpClient, CharSequence user, CharSequence pass) {
+        try {
             var success = ftpClient.login(user.toString(), pass.toString());
             if (true) {
                 ftpClient.enterLocalPassiveMode();
             }
-            return success;
-        }, e -> {
-            e.printStackTrace();
-            return false;
-        });
+            return TGS_Union.of(success);
+        } catch (IOException ex) {
+            return TGS_Union.ofExcuse(ex);
+        }
     }
 
     public static List<String> fetchReply(FTPClient ftpClient) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.tugalsan.api.ftp {
     requires org.apache.commons.net;
-    requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.callable;
     requires com.tugalsan.api.runnable;
+    requires com.tugalsan.api.union;
     requires com.tugalsan.api.list;
     requires com.tugalsan.api.stream;
     requires com.tugalsan.api.file;
