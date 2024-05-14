8104305e649034716aa064c6000adf345d39ea1b
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-05-14 07:56:12 +0300

    .

--- src/main/java/com/tugalsan/api/ftp/server/TS_FtpUtils.java
+++ src/main/java/com/tugalsan/api/ftp/server/TS_FtpUtils.java
@@ -3,12 +3,13 @@ package com.tugalsan.api.ftp.server;
 import com.tugalsan.api.file.server.TS_FileUtils;
 import com.tugalsan.api.list.client.TGS_ListUtils;
 import com.tugalsan.api.log.server.TS_Log;
-import com.tugalsan.api.stream.client.TGS_StreamUtils;
+import com.tugalsan.api.union.client.TGS_UnionExcuse;
+import com.tugalsan.api.union.client.TGS_UnionExcuseVoid;
 import com.tugalsan.api.unsafe.client.TGS_UnSafe;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Optional;
+import java.util.StringJoiner;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPReply;
 
@@ -16,72 +17,86 @@ public class TS_FtpUtils {
 
     final private static TS_Log d = TS_Log.of(TS_FtpUtils.class);
 
-    public static Optional<String> fetchWorkingDirectory(FTPClient ftpClient) {
+    public static TGS_UnionExcuse<String> fetchWorkingDirectory(FTPClient ftpClient) {
         return TGS_UnSafe.call(() -> {
-            return Optional.of(ftpClient.printWorkingDirectory());
+            return TGS_UnionExcuse.of(ftpClient.printWorkingDirectory());
         }, e -> {
-            e.printStackTrace();
-            return Optional.empty();
+            return TGS_UnionExcuse.ofExcuse(e);
         });
     }
 
-    public static boolean makeDirectory(FTPClient ftpClient, CharSequence newDir_withSlashPrefix) {
+    public static TGS_UnionExcuseVoid makeDirectory(FTPClient ftpClient, CharSequence newDir_withSlashPrefix) {
         return TGS_UnSafe.call(() -> {
-            return ftpClient.makeDirectory(newDir_withSlashPrefix.toString());
+            var result = ftpClient.makeDirectory(newDir_withSlashPrefix.toString());
+            if (!result) {
+                return TGS_UnionExcuseVoid.ofExcuse(d.className, "makeDirectory", "result is false");
+            }
+            return TGS_UnionExcuseVoid.ofVoid();
         }, e -> {
-            e.printStackTrace();
-            return false;
+            return TGS_UnionExcuseVoid.ofExcuse(e);
         });
     }
 
-    public static boolean upload(FTPClient ftpClient, Path file) {
+    public static TGS_UnionExcuseVoid upload(FTPClient ftpClient, Path file) {
         return TGS_UnSafe.call(() -> {
-            return ftpClient.storeFile(TS_FileUtils.getNameFull(file), Files.newInputStream(file));
+            var result = ftpClient.storeFile(TS_FileUtils.getNameFull(file), Files.newInputStream(file));
+            if (!result) {
+                return TGS_UnionExcuseVoid.ofExcuse(d.className, "makeDirectory", "result is false");
+            }
+            return TGS_UnionExcuseVoid.ofVoid();
         }, e -> {
-            e.printStackTrace();
-            return false;
+            return TGS_UnionExcuseVoid.ofExcuse(e);
         });
     }
 
-    public static boolean changeWorkingDirectory(FTPClient ftpClient, CharSequence newDir_withSlashPrefix) {
+    public static TGS_UnionExcuseVoid changeWorkingDirectory(FTPClient ftpClient, CharSequence newDir_withSlashPrefix) {
         return TGS_UnSafe.call(() -> {
-            return ftpClient.changeWorkingDirectory(newDir_withSlashPrefix.toString());
+            var result = ftpClient.changeWorkingDirectory(newDir_withSlashPrefix.toString());
+            if (!result) {
+                return TGS_UnionExcuseVoid.ofExcuse(d.className, "makeDirectory", "result is false");
+            }
+            return TGS_UnionExcuseVoid.ofVoid();
         }, e -> {
-            e.printStackTrace();
-            return false;
+            return TGS_UnionExcuseVoid.ofExcuse(e);
         });
     }
 
-    public static void destroy(FTPClient ftpClient) {
-        TGS_UnSafe.run(() -> ftpClient.logout(), e -> TGS_StreamUtils.runNothing());
-        TGS_UnSafe.run(() -> ftpClient.disconnect(), e -> TGS_StreamUtils.runNothing());
+    public static TGS_UnionExcuseVoid destroy(FTPClient ftpClient) {
+        var errors = new StringJoiner(" | ");
+        TGS_UnSafe.run(() -> ftpClient.logout(), e -> errors.add(e.toString()));
+        TGS_UnSafe.run(() -> ftpClient.disconnect(), e -> errors.add(e.toString()));
+        if (!errors.toString().isEmpty()) {
+            return TGS_UnionExcuseVoid.ofExcuse(d.className, "destroy", errors.toString());
+        }
+        return TGS_UnionExcuseVoid.ofVoid();
     }
 
-    public static Optional<FTPClient> connect(CharSequence hostName, int port) {
+    public static TGS_UnionExcuse<FTPClient> connect(CharSequence hostName, int port) {
         return TGS_UnSafe.call(() -> {
             var ftpClient = new FTPClient();
             ftpClient.connect(hostName.toString(), port);
             var replyCode = ftpClient.getReplyCode();
             if (!FTPReply.isPositiveCompletion(replyCode)) {
-                TGS_UnSafe.thrw(d.className, "connect", "Operation failed. Server reply code: " + replyCode);
+                TGS_UnionExcuse.ofExcuse(d.className, "connect", "Operation failed. Server reply code: " + replyCode);
             }
-            return Optional.of(ftpClient);
+            return TGS_UnionExcuse.of(ftpClient);
         }, e -> {
-            e.printStackTrace();
-            return Optional.empty();
+            return TGS_UnionExcuse.ofExcuse(e);
         });
     }
 
-    public static boolean login(FTPClient ftpClient, CharSequence user, CharSequence pass) {
+    public static TGS_UnionExcuseVoid login(FTPClient ftpClient, CharSequence user, CharSequence pass) {
         return TGS_UnSafe.call(() -> {
-            var success = ftpClient.login(user.toString(), pass.toString());
+            var result = ftpClient.login(user.toString(), pass.toString());
             if (true) {
                 ftpClient.enterLocalPassiveMode();
             }
-            return success;
+            if (!result) {
+                return TGS_UnionExcuseVoid.ofExcuse(d.className, "makeDirectory", "result is false");
+            }
+            return TGS_UnionExcuseVoid.ofVoid();
         }, e -> {
-            e.printStackTrace();
-            return false;
+            return TGS_UnionExcuseVoid.ofExcuse(e);
         });
     }
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.tugalsan.api.ftp {
     requires org.apache.commons.net;
     requires com.tugalsan.api.unsafe;
+    requires com.tugalsan.api.union;
     requires com.tugalsan.api.callable;
     requires com.tugalsan.api.runnable;
     requires com.tugalsan.api.list;
