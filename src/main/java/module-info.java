ac90322185e28fa3098ec1ec6a279df857890c7d
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-24 16:01:55 +0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module com.tugalsan.api.ftp {
     requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.union;
     requires com.tugalsan.api.callable;
-    requires com.tugalsan.api.runnable;
+    
     requires com.tugalsan.api.list;
     requires com.tugalsan.api.stream;
     requires com.tugalsan.api.file;
