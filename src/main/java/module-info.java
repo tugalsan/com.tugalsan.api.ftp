95f9dd1bbd286a1e909739d517e4aaee093ebc01
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-27 12:09:13 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.tugalsan.api.ftp {
     requires org.apache.commons.net;
     requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.union;
-    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.function;
     
     requires com.tugalsan.api.list;
     requires com.tugalsan.api.stream;
